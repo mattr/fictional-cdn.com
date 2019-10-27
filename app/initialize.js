20c1441fe584a15ae77a285fcdd11111acb05864
Matt Redmond <me@mattredmond.com>
2019-10-27 21:54:09 +1030

    Update page title and app.js message

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
-  console.log('Initialized app');
+  console.log('Hi there!');
+  console.log("We discovered that https://fictional-cdn.com wasn't actually registered though it was used in the Rails guides.");
+  console.log("So we did what any conscientious Rails developer would do: we registered it!");
 });
